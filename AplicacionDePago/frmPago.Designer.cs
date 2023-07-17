bcf5045098bece7b995874aca9d6a9a2d85fb42a
SaulDavid1998 <105959698+SaulDavid1998@users.noreply.github.com>
2023-07-16 22:10:22 -0300

    DIseño formulario Pago terminado

--- AplicacionDePago/frmPago.Designer.cs
+++ AplicacionDePago/frmPago.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmPago";
+            groupBox1 = new GroupBox();
+            rbtnEfectivo = new RadioButton();
+            rbtnTarjeta = new RadioButton();
+            label1 = new Label();
+            lstTarjetas = new ListBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            chkPredeterminado = new CheckBox();
+            btnOk = new Button();
+            btnCancelar = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbtnEfectivo);
+            groupBox1.Controls.Add(rbtnTarjeta);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(283, 66);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Facturacion";
+            // 
+            // rbtnEfectivo
+            // 
+            rbtnEfectivo.AutoSize = true;
+            rbtnEfectivo.Location = new Point(177, 29);
+            rbtnEfectivo.Name = "rbtnEfectivo";
+            rbtnEfectivo.Size = new Size(67, 19);
+            rbtnEfectivo.TabIndex = 1;
+            rbtnEfectivo.TabStop = true;
+            rbtnEfectivo.Text = "Efectivo";
+            rbtnEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbtnTarjeta
+            // 
+            rbtnTarjeta.AutoSize = true;
+            rbtnTarjeta.Location = new Point(54, 29);
+            rbtnTarjeta.Name = "rbtnTarjeta";
+            rbtnTarjeta.Size = new Size(59, 19);
+            rbtnTarjeta.TabIndex = 0;
+            rbtnTarjeta.TabStop = true;
+            rbtnTarjeta.Text = "Tarjeta";
+            rbtnTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Tipo de tarjeta";
+            // 
+            // lstTarjetas
+            // 
+            lstTarjetas.FormattingEnabled = true;
+            lstTarjetas.ItemHeight = 15;
+            lstTarjetas.Location = new Point(122, 105);
+            lstTarjetas.Name = "lstTarjetas";
+            lstTarjetas.Size = new Size(173, 94);
+            lstTarjetas.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 217);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(173, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Nro de tarjeta";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(122, 262);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(99, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(227, 262);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(68, 23);
+            comboBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Fecha expiracion";
+            // 
+            // chkPredeterminado
+            // 
+            chkPredeterminado.AutoSize = true;
+            chkPredeterminado.Location = new Point(19, 305);
+            chkPredeterminado.Name = "chkPredeterminado";
+            chkPredeterminado.Size = new Size(325, 19);
+            chkPredeterminado.TabIndex = 8;
+            chkPredeterminado.Text = "establecer como método de facturación predeterminado";
+            chkPredeterminado.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            btnOk.Location = new Point(144, 349);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(94, 27);
+            btnOk.TabIndex = 9;
+            btnOk.Text = "Ok";
+            btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.DialogResult = DialogResult.Cancel;
+            btnCancelar.Location = new Point(249, 349);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(94, 27);
+            btnCancelar.TabIndex = 10;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmPago
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(355, 388);
+            ControlBox = false;
+            Controls.Add(btnCancelar);
+            Controls.Add(btnOk);
+            Controls.Add(chkPredeterminado);
+            Controls.Add(label3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(lstTarjetas);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "frmPago";
+            Text = "frmPago";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton rbtnEfectivo;
+        private RadioButton rbtnTarjeta;
+        private Label label1;
+        private ListBox lstTarjetas;
+        private TextBox textBox1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label3;
+        private CheckBox chkPredeterminado;
+        private Button btnOk;
+        private Button btnCancelar;
     }
 }
